60b208d7799ccc017222ba177f22c975d2ac9443
Aashish Shilkande <83578134+Aashish257@users.noreply.github.com>
2025-11-30 20:42:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function highlight() {
     //Write your code here
+    const boldWords = document.querySelectorAll('strong');
 
+    // Change color to green
+    boldWords.forEach(function(word) {
+        word.style.color = "rgb(0, 128, 0)";
+    });
 
 }
 
 
 function return_normal() {
     //Write your code here
+    const boldWords = document.querySelectorAll('strong');
 
-    
+    // Change color back to black
+    boldWords.forEach(function(word) {
+        word.style.color = "rgb(0, 0, 0)";
+    });    
 }
